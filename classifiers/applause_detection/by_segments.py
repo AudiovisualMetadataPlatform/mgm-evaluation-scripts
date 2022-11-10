517d319d7b8dafea3f01da12642b2904bbbfc1c2
Rimsha Khalid <rimshakhalid992@gmail.com>
2022-11-10 16:34:45 +0500

    code cleanup

--- classifiers/applause_detection/by_segments.py
+++
@@ -1,5 +0,0 @@
-from classifiers.audio_segmentation.by_segments import BySegments as ASBySegments
-
-class BySegments(ASBySegments):
-    def __init__(self):
-        super().__init__('ApplauseDetection')
